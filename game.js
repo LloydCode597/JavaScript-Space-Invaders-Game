64bc229a6679890200f469588947ba14dd51ad40
Lloyd Allen <lloydallen597@gmail.com>
2026-02-07 22:54:07 -0600

    Add Player class and spaceship sprite
    Introduce a Player class that loads a spaceship image (img/spaceship.png), scales it, sets the player position at bottom-center, and provides a draw method. Start an animation loop that clears the canvas and renders the player each frame. Also add the spaceship image asset.

--- game.js
+++ game.js
@@ -3,3 +3,47 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+class Player {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./img/spaceship.png";
+    image.onload = () => {
+      const scale = 0.15;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height - 22,
+      };
+    };
+  }
+  draw() {
+    if (!this.image) return;
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height,
+    );
+    // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+const player = new Player();
+player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+}
+
+animate();
